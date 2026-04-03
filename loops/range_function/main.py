eef479f6bae1605af67a886b95936324533dc15b
sycwin-svg <>
2026-04-03 23:26:02 +0000

    Range Function

---
+++ loops/range_function/main.py
@@ -0,0 +1,7 @@
+# List of products on promotion for each weekday
+daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
+
+# List of weekdays corresponding to the promotions
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for day in range(5):
+    print (weekdays[day],": promotion on ",daily_promotions[day])
